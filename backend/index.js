88ffc649fbae609218eb15ff4926c3f06af2e907
Rajan Kumar <rk4009023@gmail.com>
2025-12-18 18:21:31 +0530

    Fix vulnerabilities, update start script, and cleanup index.js for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,10 @@ import userRoutes from "./routes/user.route.js";
 import companyRoutes from "./routes/compnay.route.js";
 import jobRoutes from "./routes/job.route.js";
 import applicationRoutes from "./routes/application.route.js";
-import path from "path";
 
 dotenv.config({});
 
 const app = express();
-
-const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -21,28 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'https://job-portal-4haa.onrender.com',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 };
 app.use(cors(corsOptions));
-
-// app.get('/', (req, res) => {
-//     return res.status(200).json({
-//         message: "API is working"
-//     });
-// });
 
 //api routes
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/company', companyRoutes);
 app.use('/api/v1/job', jobRoutes);
 app.use('/api/v1/application', applicationRoutes);
-
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-
-app.get("*",(_, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 4000;
 
